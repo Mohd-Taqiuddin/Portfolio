a5a375fe63335a150b7a85f4169490b90432ff66
Mohd Taqiuddin <taqiuddin12345@gmail.com>
2024-10-09 18:02:29 -0500

    done with projects and fetching data from github

--- src/portfolio.js
+++ src/portfolio.js
@@ -317,7 +317,7 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "animated_ashutosh.png",
+    profile_image_path: "taqi.png",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with React, Android, Cloud and Opensource Development.",
   }
